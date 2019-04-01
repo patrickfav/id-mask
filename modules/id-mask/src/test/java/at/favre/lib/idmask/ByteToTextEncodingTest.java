e17b60ff01abaf50b1268c59eac2c287097a9ece
pfavre <patrick.favre@rise-world.com>
2019-04-01 21:18:46 +0200

    Add ByteToTextEncoding test

---
+++ modules/id-mask/src/test/java/at/favre/lib/idmask/ByteToTextEncodingTest.java
@@ -0,0 +1,37 @@
+package at.favre.lib.idmask;
+
+import at.favre.lib.bytes.Bytes;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertArrayEquals;
+
+@RunWith(Parameterized.class)
+public class ByteToTextEncodingTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new ByteToTextEncoding.Base64()}, {new ByteToTextEncoding.Base32()}
+        });
+    }
+
+    private final ByteToTextEncoding encoding;
+
+    public ByteToTextEncodingTest(ByteToTextEncoding encoding) {
+        this.encoding = encoding;
+    }
+
+    @Test
+    public void testEncoding() {
+        for (int i = 1; i < 64; i++) {
+            byte[] random = Bytes.random(i).array();
+            String encoded = encoding.encode(random);
+            assertArrayEquals(random, encoding.decode(encoded));
+        }
+    }
+
+}
